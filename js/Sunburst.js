d5f0a27617fa9681b176dd120736542ed354ebdc
maybeLee <maybalee98@foxmail.com>
2019-08-20 00:54:43 +0800

    I have tried...

--- js/Sunburst.js
+++ js/Sunburst.js
@@ -23,9 +23,28 @@ var data = [{
 		"User", "Intrinsic Properties", "Intrinsic Properties", "Intrinsic Properties", "Digitally Extended Properties",
 		"Content Navigation", "Content Navigation", "Content Navigation", "Information Affordance", "Information Affordance"
     ],
-    outsidetextfont: { size: 20, color: "#377eb8" },
+	/*text: [
+		'', '', '', '', '', '', 'The time before a product becomes unreliable or unusable.', 
+		'', 'How bounded a system is with space.', 'How bounded a system is with time. ', 'How bounded a system is with the surrounding environment. ', 
+		'Number of ways information can be added onto a product', 
+		'', 'Interaction with a product as a physical object ', 
+		'To work with others. ', 'Responses to user actions- Good feedback is supposed to make the results of different user actions clear.', 
+		'Changing parts of product to match user preferences', 
+		'Ability to control how much of your own personal information is shared with others', 
+		'Safety of your information.', 
+		'How usable a product is different users with a wide variety of characteristics and backgrounds', 
+		'The price of purchasing and using a device.', 
+		'', 'How easy it is to change the physical shape of a product', 
+		'How easy it is to convert into a digital form.', 
+		'Viewing, examining, and understanding the information presented', 
+		'Arranging the information presented into an ordered format', 
+		'Ease of finding a specific piece of information', 
+		'The amount of information that a particular product contains', 
+		'The number of different formats of information presented.'],
+    outsidetextfont: { size: 20, color: "#377eb8" },*/
     // leaf: {opacity: 0.4},
     marker: { line: { width: 2 } },
+	
 }];
 
 var layout = {
